07294eb43fb41ddb1a21da18d08c89172bc8d4fa
lisi <lisi@qq.com>
2022-03-08 15:13:21 +0800

    first commit

---
+++ src/main/java/com/xm/boot/MainApplication.java
@@ -0,0 +1,15 @@
+package com.xm.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mintFM
+ * @create 2021-12-04 9:15
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class,args);
+    }
+}
